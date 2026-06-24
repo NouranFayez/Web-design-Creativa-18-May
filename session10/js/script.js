4ae48266b7cfee13c6e4cc79e446600ae132ec5c
NouranFayez <nouran_ahmed269@hotmail.com>
2026-06-24 17:30:55 +0300

    session10 part 1

---
+++ session10/js/script.js
@@ -0,0 +1,115 @@
+console.log('JS')
+
+// var / let / const
+
+var userName = "Nouran"
+var lastName = "Fayez"
+
+var fullName = userName + " " + lastName
+// template string
+var fullName = `first name = ${userName} 
+ last name ${lastName}`
+
+ var age = 32
+
+ var isGraduated = false
+
+ var p = null
+
+ var r 
+
+
+// console.log(p)
+// console.log(r)
+
+
+var x = "Mazen"
+var y = 30
+
+// console.log(x + y)
+// console.log(x / y)
+
+// ++ increment
+
+var t = 20
+// console.log(t++) // 20
+// console.log(t) // 21 
+// console.log(++t) // 22
+
+// // -- Decrement
+// console.log(t--) // 22
+// console.log(t) // 21 
+// console.log(--t) // 20
+
+// var k = 10
+// k = k + 5
+// console.log(k) // 15
+// console.log(k-=10) // 5
+// k = "sdfghjk"
+// console.log(k)
+
+var k = 5
+var v = 20
+var t = 5
+
+// console.log(k > v)
+// console.log(k <= t)
+
+var name = "5"
+var num = 5
+
+// console.log(name == num) // value
+// console.log(name === num) // value , type
+
+// console.log(name != num) // value false
+// console.log(name !== num) // value , type true
+
+// typeof
+
+// console.log(typeof(name))
+// console.log(typeof num )
+
+// // console.log(Name)
+// console.log(typeof name == "String")
+
+// ternary Operator ==> (condition) ? true : false
+
+
+// var p = prompt("enter your name" , "Amr")
+// console.log(p);
+// (p == "Amr") ? console.log('welcome Amr ') : console.log('invalid user')
+
+
+// var job = prompt('enter your Job')
+// var job = "" // empty string (ok)
+// var job = null //  (cancel)
+
+// console.log(job);
+
+
+// -- (name)
+// empty string
+// cancel ==> null
+
+// console.log(job == "")
+// console.log(job == null)
+
+// && 
+// console.log(job == "" &&  job== null && typeof job == "object")
+// console.log(job == "" ||   job== null || typeof job == "xfghjk")
+
+
+// falsy values
+var job = null;
+var job = 0;
+var job = -0;
+var job ;
+var job = "";
+var job = false ;
+// truthy values 
+var job = 20 ;
+var job = " ";
+var job = true;
+var job = "Developer" ;
+(!job) ? console.log('correct') : console.log('wrong')
+
