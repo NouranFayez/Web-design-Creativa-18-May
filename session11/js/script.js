eb523fc28d2308f1c30ab0e8f7b101ccd3eea35b
NouranFayez <nouran_ahmed269@hotmail.com>
2026-06-27 17:00:46 +0300

    session11 part1

---
+++ session11/js/script.js
@@ -0,0 +1,93 @@
+
+// function sum(){
+//     // console.log(10 + 20)
+
+//     return 20 + 10
+// }
+
+
+// var s = sum()
+
+// var result = sum() + 10
+
+// console.log(result)
+
+// var p = prompt()
+
+
+// function sumNumbers(num1 , num2 , num3 = "50"){
+//     console.log(num1)
+//     return num1 + num2 + num3
+// }
+
+// var result1 = sumNumbers(200 , 100 )
+
+// console.log(result1)
+
+
+// function test(endNum){
+//     for(var i = 1 ; i<=endNum ; i++){
+//         console.log(i)
+//     }
+// }
+
+
+// var p = prompt("start or not")
+
+
+// if(p == "start"){
+//     test(15)
+// }
+
+
+
+// var result 
+// function getName(){
+//     var firstName = prompt('enter first Name')
+//     var lastName = prompt('enter last name')
+
+//     result = `${firstName} ${lastName}`
+// }
+
+
+// getName()
+
+
+// console.log(result)
+
+
+// function statment
+// function test(){
+//     return "js"
+// }
+
+// var result = test()
+
+// function expression 
+// var result = function(){
+//     return "Js"
+// }
+
+// console.log(result())
+
+
+// arrow Function 
+// var res = (num)=>{
+//     return num > 30 && typeof num == "string"
+// }
+
+var res = num => num > 30 && typeof num == "string"
+
+
+
+
+console.log(res(20))
+
+
+
+
+// function looping (startNum , endNum , breakNum , contNum)
+
+
+// looping(1 , 10 , 8 , 3)
+// looping(1 , 10)
