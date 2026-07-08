682005ae3629b95170c671cbe9a2784ea45faad1
NouranFayez <nouran_ahmed269@hotmail.com>
2026-07-08 17:38:52 +0300

    session15

--- session15/js/script.js
+++ session15/js/script.js
@@ -19,9 +19,75 @@ let handleCategory = (type) => {
     }
     else {
         for (let i = 0; i < category2.length; i++) {
-            category1[i].classList.remove('d-none')
+            category1[i].classList.add('d-none')
             category2[i].classList.add('d-block')
         }
     }
 }
+
+
+
+// let arr = [2 ,3, 4, 5, 10]
+
+// arr.forEach((ele , i , arr)=>{
+
+// })
+
+
+
+
+let myFunc = (val , callBack)=>{
+    setTimeout(()=>{
+        if(typeof val == "number")
+            callBack(val*2 , true)
+        else callBack('invalid number' , false)
+    }, 2000)
+}
+
+// myFunc("test" , (res , flag)=>{
+//     console.log(res)
+//     console.log(flag)
+// })
+
+
+// let date = new Date()
+
+// promise ==> pending  
+            // fulfilled
+            // reject
+
+let myPromise = (val)=>{
+    return new Promise((resolve , reject)=>{
+        if(typeof val == "number")
+            resolve(val*2 , true)
+        else reject('invalid number' , false)
+    })
+}
+
+// console.log(myPromise("sdfghjk"))
+
+// then catch 
+myPromise("dfghjkl").then((result)=>{
+    console.log(result)
+}).catch((err)=>{
+    console.log(err)
+})
+
+
+fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response =>response.json())
+      .then(json => console.log(json))
+      .catch(err=>console.log(err))
+
+
+
+//  callBack 
+//  promise ==> then catch ||  async await
+
+
+// advanced js
+// typescript 
+//  angular - react
+
+
 
